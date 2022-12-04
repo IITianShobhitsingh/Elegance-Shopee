d75eef4e6c9e28a862da04e8e2794cbcdf53b791
Shobhit Singh <shobhitsingh25032003@gmail.com>
2022-12-04 18:14:12 +0530

    files added

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+//search filter
 var btn = document.querySelector('#button')
-var data = document.querySelector('.search').value
+var data = document.querySelector('.search')
 btn.addEventListener('click',function(e){
-    console.log(data);
+    console.log(data.value);
+})
+
+
+//for directing to affilated site
+var shift1 = document.querySelector('#btn1')
+shift1.addEventListener('click',function(e){
+    location.href="https://amazon.com";
 })
