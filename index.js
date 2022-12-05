4dcb25fc1a6a69a96545eafa3cee37d405ee95ec
Shobhit Singh <shobhitsingh25032003@gmail.com>
2022-12-05 18:50:56 +0530

    modified

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-//search filter
-var btn = document.querySelector('#button')
-var data = document.querySelector('.search')
-btn.addEventListener('click',function(e){
-    console.log(data.value);
-})
-
-
-//for directing to affilated site
-var shift1 = document.querySelector('#btn1')
-shift1.addEventListener('click',function(e){
-    location.href="https://amazon.com";
+var click = document.querySelector('#btn')
+click.addEventListener('click',function(){
+    location.href= "menu.html";
 })
