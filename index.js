ab9c985ff2c92106c4f40920085bca584c0193ee
Shobhit Singh <shobhitsingh25032003@gmail.com>
2022-12-08 17:01:47 +0530

    fresh

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
- 
+ //for home button
+ var item_0 = document.querySelector('#home')
+ item_0.addEventListener('click',function(){
+    location.href = "index.html";
+ })
+
+ //for about us 
+ var item_4 = document.querySelector('#about')
+ item_4.addEventListener('click',function(){
+    location.href = 'about.html';
+ })
+
+ //for contact us 
+ var item_3 = document.querySelector('#contact')
+ item_3.addEventListener('click',function(){
+    location.href = 'contact.html';
+ })
+
+ //for brands
+ var item_21 = document.querySelector('#about')
+ item_4.addEventListener('click',function(){
+    location.href = 'about.html';
+ })
