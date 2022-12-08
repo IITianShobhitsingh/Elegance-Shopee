3666af12d269ca487444b36d01cb2e8f2d97a96c
Shobhit Singh <shobhitsingh25032003@gmail.com>
2022-12-08 17:42:08 +0530

    rendering

--- index.js
+++ index.js
@@ -17,7 +17,28 @@
  })
 
  //for brands
- var item_21 = document.querySelector('#about')
- item_4.addEventListener('click',function(){
-    location.href = 'about.html';
+ var nykaa = document.querySelector('#nykaa')
+ nykaa.addEventListener('click',function(){
+    location.href = 'https://juniper-mail-4e7.notion.site/0d2c3ac701cc4ff1893193c58954aacf?v=4a35ce12371f4b438a3a55dabaa82ca0';
+ })
+
+ var mamaearth = document.querySelector('#mama')
+ mamaearth.addEventListener('click',function(){
+    location.href = 'https://juniper-mail-4e7.notion.site/0d2c3ac701cc4ff1893193c58954aacf?v=5da8cf1d167247a7af24b3343f79b2f2';
+ })
+
+ var mca = document.querySelector('#mc')
+ mca.addEventListener('click',function(){
+    location.href = 'https://juniper-mail-4e7.notion.site/0d2c3ac701cc4ff1893193c58954aacf?v=e03e2289773248b6bf0348f35ed82aa8';
+ })
+
+ var nivea = document.querySelector('#nivea')
+ nivea.addEventListener('click',function(){
+    location.href = 'https://juniper-mail-4e7.notion.site/0d2c3ac701cc4ff1893193c58954aacf?v=1bfe35380d0144928edbb6832e8dbb0f';
+ })
+
+ //for main pg of wonders
+ var pg = document.querySelector('#shop')
+ pg.addEventListener('click',function(){
+    location.href = 'https://juniper-mail-4e7.notion.site/0d2c3ac701cc4ff1893193c58954aacf?v=67f9173466544e78ace8b86c7007464f';
  })
