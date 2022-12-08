af11d9a2a6f6e6a7b9f97ee1a8eaa98d28393ca4
Shobhit Singh <shobhitsingh25032003@gmail.com>
2022-12-08 15:29:24 +0530

    re-designing

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-var click = document.querySelector('#btn')
-click.addEventListener('click',function(){
-    location.href= "https://juniper-mail-4e7.notion.site/0d2c3ac701cc4ff1893193c58954aacf?v=67f9173466544e78ace8b86c7007464f";
-})
+ 
