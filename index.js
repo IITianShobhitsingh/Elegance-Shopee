9fbc1c0fa615763e04173e48fb420f1ebf895721
Shobhit Singh <shobhitsingh25032003@gmail.com>
2022-12-09 18:35:21 +0530

    done

--- index.js
+++ index.js
@@ -38,7 +38,7 @@
  })
 
  //for main pg of wonders
- var pg = document.querySelector('#shop')
+ var pg = document.querySelector('.button-30')
  pg.addEventListener('click',function(){
     location.href = 'https://juniper-mail-4e7.notion.site/0d2c3ac701cc4ff1893193c58954aacf?v=67f9173466544e78ace8b86c7007464f';
  })
